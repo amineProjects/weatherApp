a8dc8c3e432ddebf0a4ac4c18d8fea63b51086b0
amine <benahmed.education@gmail.com>
2022-02-03 16:44:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Forcast</h1>
+    </div>
+  );
+}
+
+export default App;
